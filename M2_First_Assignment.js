565042d9ade6138a75cc369cb72559d077fa41fc
Marcus Ceradini <mnceradini@my.waketech.edu>
2025-01-14 13:43:58 -0500

    Assignment done

---
+++ M2_First_Assignment.js
@@ -0,0 +1,5 @@
+console.dir(document)
+window.onload = function(){
+    alert("Hello")
+}
+document.getElementById("results").textContent = "JavaScript is Cool";
--- index.js
+++ index.js
@@ -21,11 +21,33 @@ messageElement.textContent = `Hello, ${student}`;
 document.body.appendChild(messageElement)
 
 // WRITE TO ELEMENT BY IDs //
-document.getElementById("myH1").innerHTML = "I hate JavaScript";
-document.getElementById("myParagraph").textContent = "I have Java";
+// document.getElementById("myH1").innerHTML = "I hate JavaScript";
+// document.getElementById("myParagraph").textContent = "I have Java";
 
 // STYLING page elements //
 
-document.body.style.color = "green";
-document.body.style.backgroundColor = "black";
-document.body.style.fontSize = "2em"
+// document.body.style.color = "green"
+// document.body.style.backgroundColor = "black"
+// document.body.style.fontSize = "2em"
+
+// EVENT LISTENERS //
+document.getElementById("myH1").addEventListener("click", function(){
+    document.getElementById("myH1").style.color = "red"
+});
+
+document.getElementById("myH1").addEventListener("mouseover", function(){
+    document.getElementById("myH1").style.color = "orange"
+})
+
+document.getElementById("myParagraph").addEventListener("dblclick", function(){
+    document.getElementById("myParagraph").style.fontSize = "2em"
+})
+
+document.getElementById("myButton").addEventListener("click", function(){
+    document.body.style.backgroundColor = "black";
+    document.getElementById("myParagraph").style.color = "White"
+});
+
+// Difference between .innerHTML and .textContent
+console.log(document.getElementById("myParagraph").innerHTML)
+console.log(document.getElementById("myParagraph").textContent)
